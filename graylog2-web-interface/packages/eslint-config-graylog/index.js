ad5ce821305f5c368d9dc2d4063868fa955bb3de
Kyle Knight <kyle@graylog.com>
2020-06-22 08:17:37 -0500

    Updated eslint config

--- graylog2-web-interface/packages/eslint-config-graylog/index.js
+++ graylog2-web-interface/packages/eslint-config-graylog/index.js
@@ -52,11 +52,36 @@ module.exports = {
 
     'padding-line-between-statements': [
       'error',
-      { blankLine: 'always', prev: '*', next: 'return' },
-      { blankLine: 'always', prev: ['const', 'let', 'var'], next: '*' },
-      { blankLine: 'any', prev: ['const', 'let', 'var'], next: ['const', 'let', 'var'] },
-      { blankLine: 'always', prev: 'directive', next: '*' },
-      { blankLine: 'any', prev: 'directive', next: 'directive' },
+      {
+        blankLine: 'always',
+        prev: ['block', 'block-like', 'cjs-export', 'class', 'export', 'import'],
+        next: '*',
+      },
+      {
+        blankLine: 'always',
+        prev: '*',
+        next: ['block', 'block-like'],
+      },
+      {
+        blankLine: 'always',
+        prev: ['let', 'const'],
+        next: ['multiline-const', 'multiline-let', 'block-like'],
+      },
+      {
+        blankLine: 'always',
+        prev: '*',
+        next: 'return',
+      },
+      {
+        blankLine: 'any',
+        prev: ['export', 'import'],
+        next: ['export', 'import'],
+      },
+      {
+        blankLine: 'never',
+        prev: ['let', 'const'],
+        next: ['singleline-let', 'singleline-const'],
+      },
     ],
 
     // eslint-plugin-flowtype configs, `recommended` is too weak in a couple of places:
